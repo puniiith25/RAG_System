7deae2b977e3cadfd9257eda203bf35a84365750
puniiith25 <puniiith25@gmail.com>
2026-08-11 00:39:17 +0530

    chunks

--- Backend/main.py
+++ Backend/main.py
@@ -1,18 +1,18 @@
 from fastapi import  FastAPI ,UploadFile ,File , HTTPException
 from pypdf import PdfReader
+from services.text_splitter import split_text
+
 import os
 
 
 app = FastAPI()
 
-# To Store a User Uploaded File in this Uploads Folder Directory# To Store a User Uploaded File in this Uploads Folder Directory
-
-# To Store a User Uploaded File in this Uploads Folder Directory# To Store a User Uploaded File in this Uploads Folder Directory
+# To Store a User Uploaded File in this Uploads Folder Directory
 
 UPLOAD_DIR="Uploads"
 os.makedirs(UPLOAD_DIR,exist_ok=True)
 
-@app.get("/Healt")
+@app.get("/healt")
 def Healt():
     return {"Message:RAG System API is Working"}
 
@@ -23,7 +23,23 @@ async def Upload_PDF(file:UploadFile = File(...)):
             status_code=400,
             detail="Only PDF files are allowed"
                 )
-    file_path = os.path.join(Upload_PDF,file.filename)
+    file_path = os.path.join(UPLOAD_DIR,file.filename)
     with open(file_path,"wb") as buffer:
         content = await file.read()
         buffer.write(content)
+    reader = PdfReader(file_path)
+    text =""
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text+'\n'
+    chunks = split_text(text)
+
+
+    return {
+        "filename": file.filename,
+        "pages": len(reader.pages),
+        "total_characters": len(text),
+        "total_chunks": len(chunks),
+        "chunks": chunks
+    }
